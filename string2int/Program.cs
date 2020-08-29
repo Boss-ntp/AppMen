4006b6d09b68b72c9e49eda47e6717cb768ec88f
Nattapong Nuchmuang <boss_ntp@outlook.com>
2020-08-29 13:41:16 +0700

    First Commit

---
+++ string2int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string2int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = null;
+            string number = null;
+            Console.Write("Enter text : ");
+            text = Console.ReadLine();
+            char[] textChar = text.ToCharArray();
+            foreach(char c in textChar)
+            {
+                if (Char.IsNumber(c))
+                {
+                    number += c;
+                }
+            }
+            Console.WriteLine("Output : " + number);
+            Console.Read();
+        }
+    }
+}
